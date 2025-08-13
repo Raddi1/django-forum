8e9adb0d3584c1ad694e687a257f581f5679cceb
triks476 <dmitropekka@gmail.com>
2025-08-13 21:41:39 +0300

    Update views.py

--- main/views.py
+++ main/views.py
@@ -107,28 +107,4 @@ def error_page(request, exception=None):
     return render(request, 'main/error_page.html', status=500)
 
 
-@login_required
-def commentarii(request, pk):
-    thread = get_object_or_404(Thread, pk=pk)
-    comments = thread.comments.filter(parent__isnull=True)
-
-    if request.method == 'POST':
-        form = CommentForm(request.POST)
-        if form.is_valid():
-            comment = form.save(commit=False)
-            comment.thread = thread
-            comment.author = request.user
-            parent_id = request.POST.get('parent_id')
-            if parent_id:
-                comment.parent = Comment.objects.get(id=parent_id)
-            comment.save()
-            return redirect('thread-detail', pk=thread.pk)
-    else:
-        form = CommentForm()
-
-    return render(request, 'main/commentarii.html', {
-        'thread': thread,
-        'comments': comments,
-        'form': form
-    })
 
